121385baea353bfb7cd85471a8d46e7ffd0c11d2
Luigi <luigitosin@gmail.com>
2020-05-16 19:31:44 -0300

    Criação das interfaces

---
+++ src/modelo/dao/VendedorDao.java
@@ -0,0 +1,19 @@
+package modelo.dao;
+
+import java.util.List;
+
+import modelo.entidades.Vendedor;
+
+public interface VendedorDao {
+
+	void inserir(Vendedor vendedor);
+
+	void atualizar(Vendedor vendedor);
+
+	void deletarPorId(Integer id);
+
+	Vendedor buscarPorId(Integer id);
+
+	List<Vendedor> buscarTodos();
+
+}
